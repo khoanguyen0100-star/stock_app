d550e230cae342f2907ffae595d7ee373c33383c
khoanguyen0100-star <khoanguyen0100@gmail.com>
2026-05-15 21:53:29 +0700

    Update app_11.py

--- app_11.py
+++ app_11.py
@@ -99,7 +99,7 @@ def generate_ai_analysis(
         """
 
         response = client.chat.completions.create(
-            model="llama3-70b-8192",
+            model="llama-3.3-70b-versatile",
             messages=[
                 {
                     "role": "user",
